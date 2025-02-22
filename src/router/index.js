4e0240178e74cded97a4c1b26f2429278827597c
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2025-02-22 17:35:39 -0500

    cambios realizados

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import CameraList from '@/components/CameraList.vue';
+import UserManagement from '@/components/UserManagement.vue';
+import EventView from '@/components/EventView.vue'; // Tu componente actual
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  }
+  { path: '/', component: EventView }, // Ruta por defecto
+  { path: '/cameras', component: CameraList },
+  { path: '/users', component: UserManagement },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 export default router;
